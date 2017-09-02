5daa5223dc53b3cafe423f7aa27c021f6d34652a
michaelmay <maydev99@gmail.com>
2017-09-02 10:04:14 -0700

    Fixed single digit minute issue Fixed AM PM issue Updating is still a problem

--- app/src/main/java/com/droidloft/hometime/NewAppWidget.java
+++ app/src/main/java/com/droidloft/hometime/NewAppWidget.java
@@ -3,6 +3,7 @@ package com.droidloft.hometime;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -25,18 +26,30 @@ public class NewAppWidget extends AppWidgetProvider {
         Calendar cal = Calendar.getInstance();
         int currentHour = cal.get(Calendar.HOUR);
         int currentMinute = cal.get(Calendar.MINUTE);
+        String homeMinuteStr;
+        if(currentMinute < 10) {
+            homeMinuteStr = "0" + Integer.toString(currentMinute);
+        } else {
+            homeMinuteStr = Integer.toString(currentMinute);
+        }
         int currentAmPm = cal.get(Calendar.AM_PM);
+
+        if(currentAmPm == 1) {
+            currentHour = currentHour + 12;
+        }
         int homeTimeHour = currentHour + hoursDiff;
 
         String ampmStr;
 
-        if(currentAmPm == 1){
+        if(homeTimeHour > 12) {
             ampmStr = "PM";
+            homeTimeHour = homeTimeHour - 12;
         } else {
             ampmStr = "AM";
         }
 
-        String homeTime = homeTimeHour + ":" + currentMinute + " " + ampmStr;
+
+        String homeTime = homeTimeHour + ":" + homeMinuteStr + " " + ampmStr;
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
         views.setTextViewText(R.id.timeTextView, homeTime);
@@ -66,18 +79,33 @@ public class NewAppWidget extends AppWidgetProvider {
         Calendar cal = Calendar.getInstance();
         int currentHour = cal.get(Calendar.HOUR);
         int currentMinute = cal.get(Calendar.MINUTE);
+        String homeMinuteStr;
+        if(currentMinute < 10) {
+            homeMinuteStr = "0" + Integer.toString(currentMinute);
+        } else {
+            homeMinuteStr = Integer.toString(currentMinute);
+        }
         int currentAmPm = cal.get(Calendar.AM_PM);
+
+        if(currentAmPm == 1) {
+            currentHour = currentHour + 12;
+        }
         int homeTimeHour = currentHour + hoursDiff;
 
         String ampmStr;
 
-        if(currentAmPm == 1){
+        if(homeTimeHour >= 12) {
             ampmStr = "PM";
+            if(homeTimeHour > 12){
+                homeTimeHour = homeTimeHour - 12;
+            }
+
         } else {
             ampmStr = "AM";
         }
 
-        String homeTime = homeTimeHour + ":" + currentMinute + " " + ampmStr;
+
+        String homeTime = homeTimeHour + ":" + homeMinuteStr + " " + ampmStr;
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
         views.setTextViewText(R.id.timeTextView, homeTime);
@@ -88,6 +116,13 @@ public class NewAppWidget extends AppWidgetProvider {
 
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
+
+        ComponentName thisWidget = new ComponentName(context,NewAppWidget.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(thisWidget, views);
+
+
+
 
         appWidgetManager.updateAppWidget(appWidgetIds, views);
     }
